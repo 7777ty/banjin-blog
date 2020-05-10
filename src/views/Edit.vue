cc5984349ed7d1db6fb79a1a621499cc6dc495bf
7777ty <568318359@qq.com>
2020-05-10 20:02:15 +0800

    完成了编辑页面

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -5,18 +5,22 @@
             <div class="title">创建文章</div>
             <div class="articleTitle">
                 <p>文章标题</p>
-                <el-input v-model="articleTitle" maxlength="20" placeholder="请输入标题" show-word-limit/>
+                <el-input v-model="blogDetails.title" maxlength="20" placeholder="请输入标题" show-word-limit/>
             </div>
             <div class="articleIntroduction">
                 <p>文章简介</p>
-                <el-input rows="3" resize="none" type="textarea" v-model="articleIntroduction" placeholder="请输入文章简介"/>
+                <el-input rows="3" resize="none" type="textarea" v-model="blogDetails.description" placeholder="请输入文章简介"/>
             </div>
             <div class="articleContent">
                 <p>文章内容</p>
-                <el-input rows="12" resize="none" type="textarea" v-model="articleContent" placeholder="请输入文章内容"/>
+                <el-input rows="12" resize="none" type="textarea" v-model="blogDetails.content" placeholder="请输入文章内容"/>
+            </div>
+            <div>
+                <label>是否展示到首页</label>
+                <el-switch v-model="blogDetails.atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
             </div>
             <div class="buttons">
-                <el-button type="success">保存</el-button>
+                <el-button type="success" @click="toSaveChange">保存</el-button>
                 <el-button type="danger"> 取消</el-button>
             </div>
 
@@ -31,13 +35,29 @@
     import {Component} from 'vue-property-decorator';
     import Header2 from '@/components/Header2.vue';
     import Footer from '@/components/Footer.vue';
+    import blog from '@/api/blog';
     @Component({
         components: {Footer, Header2}
     })
+
     export default class Edit extends Vue {
-        articleTitle?: string='';
-        articleIntroduction?: string='';
-        articleContent?: string='';
+
+      blogDetails={};
+
+      toSaveChange(){
+        const Eblog=this.blogDetails;
+        blog.updateBlog({blogId:Eblog.id},{title:Eblog.title,content:Eblog.content,description:Eblog.description,atIndex:Eblog.atIndex})
+          .then(res => {
+            this.$message.success(res.msg);
+            this.$router.push({ path: `/details/${res.data.id}`})
+          })
+      }
+
+      created(){
+        blog.getDetail(this.$route.params.blogId).then(res=>{
+          this.blogDetails=res.data;
+        });
+      }
     }
 </script>
 
